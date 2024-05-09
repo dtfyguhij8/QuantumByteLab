666211338eb81d17fc713c6787b00b52f136cdde
dtfyguhij8 <js1mane1183@hotmail.com>
2024-05-09 20:19:26 +0700

    hello

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function minMeetingRoomsII(intervals) {
-  const startTimes = intervals
-    .map((interval) => interval[0])
-    .sort((a, b) => a - b);
-  const endTimes = intervals
-    .map((interval) => interval[1])
-    .sort((a, b) => a - b);
-  let rooms = 0;
-  let endIdx = 0;
-  for (let i = 0; i < startTimes.length; i++) {
-    if (startTimes[i] < endTimes[endIdx]) rooms++;
-    else endIdx++;
-  }
-  return rooms;
+function largestNumber(nums) {
+  if (nums.every((num) => num === 0)) return "0";
+  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
 }
