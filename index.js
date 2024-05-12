482cdeb0bc340db643ba9921aa1421c9acc4312b
dtfyguhij8 <js1mane1183@hotmail.com>
2024-05-12 15:08:46 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const binarySearch = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
   }
-  return -1;
-};
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
