0975acf86c3a8cfb9477360774200281d35e76a1
dtfyguhij8 <js1mane1183@hotmail.com>
2024-05-15 00:29:59 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
+function buildTree(preorder, inorder) {
+  if (preorder.length === 0 || inorder.length === 0) return null;
+  const rootVal = preorder[0];
+  const root = new TreeNode(rootVal);
+  const index = inorder.indexOf(rootVal);
+  const leftInorder = inorder.slice(0, index);
+  const rightInorder = inorder.slice(index + 1);
+  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
+  const rightPreorder = preorder.slice(1 + leftInorder.length);
+  root.left = buildTree(leftPreorder, leftInorder);
+  root.right = buildTree(rightPreorder, rightInorder);
+  return root;
 }
